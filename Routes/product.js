2b9f6f446245d86210a98a2aa0b172b9163d4381
xyfer17 <nav3044999@gmail.com>
2023-06-11 00:25:56 +0530

    latest update

--- Routes/product.js
+++ Routes/product.js
@@ -11,7 +11,73 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   try {
     const savedProduct = await newproduct.save();
     res.status(201).json(savedProduct);
-  } catch (error) {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//update product
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Delete
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const deleteProduct = await product.findByIdAndDelete(req.params.id);
+    res.status(200).json("product is deleted Successfully...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get Product
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const Product = await product.findById(req.params.id);
+    res.status(200).json(Product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get all User
+
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+
+  try {
+    let Products;
+
+    if (qNew) {
+      Products = await product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      Products = await product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      Products = await product.find();
+    }
+
+    res.status(200).json(Products);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const userRoute = require("./Routes/user");
 const authRoute = require("./Routes/auth");
 const productRoute = require("./Routes/product");
+const cartRoute = require("./Routes/cart");
+const orderRoute = require("./Routes/order");
 
 require("dotenv").config();
 
@@ -22,6 +24,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/cart", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("sever started");
